f79df9acfd1b52939c9399f56578939e4d0edbb7
rafaelmoreno21 <rafaelmoreno210900@gmail.com>
2020-09-14 21:14:56 -0400

    first-page

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
+const path = require("path");
+//settings
+app.set("port", process.env.PORT || 3000);
+app.set("views", path.join(__dirname, "views"));
+app.engine("html", require("ejs").renderFile);
+app.set("view engine", "ejs");
+
+//middlewares
+app.use(morgan("dev"));
+//Routes
+app.use(require("./routes/index"));
+//static files
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.listen(app.get("port"), () => {
+  console.log("Server on port", app.get("port"));
+});
